54f32fb3891ef35c0d21ccf5787ab272d2388b9b
BrightSt <dongmingsheng20@163.com>
2018-12-06 21:05:55 +0800

    添加单例模式 简单工厂模式

---
+++ bees-admin/src/main/java/com/dms/bees/core/aop/TestAOP.java
@@ -0,0 +1,24 @@
+package com.dms.bees.core.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TestAOP {
+
+    @Pointcut(value = "@annotation(com.dms.bees.core.common.annotion.Permission)")
+    private void cutPermission() {
+
+    }
+
+    @Around("cutPermission()")
+    public Object doPermission(ProceedingJoinPoint point) throws Throwable {
+        System.out.println(111);
+        return null;
+    }
+
+}
